54405639eaa8c4daf7e1923c5d3f8113d12f1a99
Behruz <bboisxonov@yandex.com>
2023-12-05 15:56:21 +0800

    new todo's style updated

--- src/Components/newTodo/newTodo.jsx
+++ src/Components/newTodo/newTodo.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import "./newTodo.css"
 
 function NewTodo({ children, todo, handleDelete, handleChecked }) {
     return (
         <li style={{ textDecoration: todo.isCompleted ? 'line-through' : 'none' }} className='newItem'>
             <input type="checkbox" data-todo-id={todo.id} onClick={handleChecked} defaultChecked={todo.isCompleted} />
             {children}
-            <button data-todo-id={todo.id} onClick={handleDelete}>&times;</button>
+            <button className='remove-sign__todo' data-todo-id={todo.id} onClick={handleDelete}>&times;</button>
         </li>
     )
 }
